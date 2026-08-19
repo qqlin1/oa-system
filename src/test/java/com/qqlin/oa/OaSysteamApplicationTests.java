40ca262bfbb9327beda9f52d260d16c3c7af8a1d
zxlin <2971213021@qq.com>
2026-08-19 22:15:07 +0800

    chore: initialize Spring Boot project

---
+++ src/test/java/com/qqlin/oa/OaSysteamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qqlin.oa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OaSysteamApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
